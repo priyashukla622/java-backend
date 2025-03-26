662fbda57379d2362e2e23e8eee52045e6172a96
priya shukla <priyashukla22@navgurukul.org>
2025-03-26 19:01:29 +0530

    added data in api

--- controllers/addCourse.js
+++ controllers/addCourse.js
@@ -1,27 +1,24 @@
 import Course from "../models/courseModel.js";
 const addCourse=async(req,res)=>{
     try{
-        const{title,subjects,teacher,description,duration,studentsEnrolled,topics,difficulty,price}=req.body;
-        if(!title ||!subjects|| !teacher || !description ||!duration || !studentsEnrolled ||!topics ||!difficulty ||!price){
+        const{title,teacher,teacherName,duration,studentsEnrolled,status}=req.body;
+        if(!title ||!teacher ||!teacherName||!duration || !studentsEnrolled||!status){
             return res.status(400).json({messase:"All field is require"})
         }
         const courseData=new Course({
             title,
-            subjects,
             teacher,
-            description,
+            teacherName,
             duration,
             studentsEnrolled,
-            topics,
-            difficulty,
-            price
+            status,
         });
         await courseData.save();
         res.status(201).json({message:"course added successfully"})
     }
     catch(error){
         console.log("Error is comming")
-        res.status(500).json({message:"something went wrong"})
+        res.status(500).json({message:"something went wrong", error:error.message})
     }
 };
 export default addCourse;
--- models/courseModel.js
+++ models/courseModel.js
@@ -1,42 +1,33 @@
 import mongoose from "mongoose";
+
 const courseSchema = new mongoose.Schema({
   title: { 
     type: String, 
     required: true 
-}, 
-  subjects: [{ 
-    type: String, 
-    required: true 
-}],
+  }, 
   teacher: { 
     type: mongoose.Schema.Types.ObjectId, 
     ref: "Teacher", 
     required: true 
-}, 
-  description: { 
-    type: String 
-}, 
+  }, 
+  teacherName: {
+    type: String,
+    required: true 
+  },
   duration: { 
     type: Number, 
     required: true 
-}, 
+  }, 
   studentsEnrolled: [{ 
     type: mongoose.Schema.Types.ObjectId, 
     ref: "Student" 
-}], 
-  topics: [{ 
-    type: String
-}], 
-  difficulty: { 
+  }], 
+  status: { 
     type: String, 
-    enum: ["Beginner", "Intermediate", "Advanced"], 
-    required: true 
-},
-  price: { 
-    type: Number, 
-    default: 0 
-}, 
-
+    enum: ["Pending", "Completed"], 
+    default: "Pending" 
+  },
 });
+
 const Course = mongoose.model("Course", courseSchema);
 export default Course;
